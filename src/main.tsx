aacc7976c31a826b075d6f608700d8c64a81db90
Mariangel Ramirez <mramirez@trimac.com>
2025-04-10 11:04:14 -0600

    adding <RCMentions> in main app

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactMentionExample from './ReactMentionExample.tsx'
 import './index.css'
+import RCMentionsExample from './RCMentionsExample.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ReactMentionExample />
+    <></>
+    <RCMentionsExample />
   </StrictMode>,
 )
