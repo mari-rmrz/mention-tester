d5b489e5ce6e355dc903a2f5fbfa41bb4701a3d2
Mariangel Ramirez <mariangelrmrz@gmail.com>
2025-04-11 14:30:35 -0600

    primereact not working, deleting from code

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 
 import ReactMentionExample from './ReactMention/ReactMentionExample.tsx'
 import RCMentionsExample from './RCMention/RCMentionsExample.tsx'
-import PrimeReactExample from './PrimeReact/PrimeReactExample.tsx'
 
 import './styles/index.css'
 
@@ -12,7 +11,5 @@ createRoot(document.getElementById('root')!).render(
     <ReactMentionExample />
     <hr className="dotted" />
     <RCMentionsExample />
-    <hr className="dotted" />
-    <PrimeReactExample />
   </StrictMode>,
 )
